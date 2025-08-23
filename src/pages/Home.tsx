510dd7f1890035369e31474410e42ad700ab41c0
john mckinney <30315589+john102m@users.noreply.github.com>
2025-08-23 16:22:33 +0100

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,7 @@ const Home: React.FC = () => {
           This is your starting point. Edit <code>Home.tsx</code> and make it yours!
         </p>
         <p className="text-gray-600 mb-6">
-          Start coding willy nilly. Enjoy!
+          Start coding willy nilly. Enjoy a lot!
         </p>
         <button onClick={handleClick} className="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400">
           Click Me
