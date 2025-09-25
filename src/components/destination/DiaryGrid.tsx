6ad7be12e74433a73de567289db60bcf4e1daff9
john mckinney <30315589+john102m@users.noreply.github.com>
2025-09-25 22:19:16 +0100

    other teak

--- src/components/destination/DiaryGrid.tsx
+++ src/components/destination/DiaryGrid.tsx
@@ -29,7 +29,7 @@ const DiaryGrid: React.FC<Props> = ({ tripId, tripName }) => {
     return (
         <>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
                 {entries.map((entry) => (
                     <DiaryEntryCard
                         key={entry.id}
