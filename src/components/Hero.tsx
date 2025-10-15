581c76782bcd2a9180ca62a92972aa9e21438935
john mckinney <30315589+john102m@users.noreply.github.com>
2025-10-15 22:36:21 +0100

    p

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,40 +2,39 @@
 import { NavLink } from "react-router-dom";
 export default function Hero() {
   return (
-    <header
-      className="relative h-[400px] sm:h-[450px] md:h-[500px] lg:h-[550px] bg-center bg-cover flex items-center justify-center"
-      style={{
-        backgroundImage:
-          "url('https://myjohnblogimages.blob.core.windows.net/images/croatia.webp')",
-      }}
-    >
-      <div className="bg-black/40 p-6 rounded-xl text-center max-w-xl mx-4">
-        <h1 className="text-[#f4d0e5] font-serif font-extralight drop-shadow-xl">
-          Itinera
-        </h1>
+<header
+  className="w-full aspect-[4/1] bg-center bg-contain bg-no-repeat mt-20"
+  style={{
+    backgroundImage: "url('/logo.png')",
+  }}
+>
+
+  {/* Optional content here */}
 
 
-          <NavLink
-            to="/dashboard"
-            className="
-              text-blue-900 
-              bg-blue-300 
-              rounded-2xl 
-              pt-1 pl-3 pr-3 pb-1 
-              font-bold 
-              text-lg 
-              hover:text-blue-700 
-              hover:bg-blue-200 
-              transition-colors 
-              shadow-sm
-              active:scale-95
-              focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-1
-            "
-          >
-            Dashboard
-          </NavLink>
 
-      </div>
+
+        <NavLink
+          to="/dashboard"
+          className="
+            hidden md:block
+            text-blue-900 
+            bg-blue-300 
+            rounded-2xl 
+            pt-1 pl-3 pr-3 pb-1 
+            font-bold 
+            text-lg 
+            hover:text-blue-700 
+            hover:bg-blue-200 
+            transition-colors 
+            shadow-sm
+            active:scale-95
+            focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-1
+          "
+        >
+          Dashboard
+        </NavLink>
+
 
     </header>
   );
